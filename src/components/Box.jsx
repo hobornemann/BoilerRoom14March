2fbd3d023e108c3b48a72a59639c83e12b2aea3c
WilliamBostrom <133894488+WilliamBostrom@users.noreply.github.com>
2024-03-14 13:36:05 +0100

    push

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,9 +1,12 @@
 import "./Game.css";
 import { useState } from "react";
 
-function Box({ id, onClick }) {
-
-    return <div className="box" id={id} onClick={onClick}></div>;
+function Box({ id, onClick, currentPlayer }) {
+  return (
+    <div className="box" id={id} onClick={() => onClick(id)}>
+      {currentPlayer ? "hej" : "hejdå"}
+    </div>
+  );
 }
 
 export default Box;
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -3,39 +3,56 @@ import Box from "./Box";
 import { useState } from "react";
 
 function Game() {
-    const [currentPlayer, setCurrentPlayer] = useState(true);
-    const [player1, setPlayer1] = useState([]);
-    const [player2, setPlayer2] = useState([]);
+  const [currentPlayer, setCurrentPlayer] = useState(true);
+  const [turns, setTurns] = useState(0);
+  const [player1, setPlayer1] = useState([]);
+  const [player2, setPlayer2] = useState([]);
 
-    const handleClick = (e) => {
-        console.log(e.target.id);
-        if (currentPlayer) {
-            console.log("player 1:");
-            setCurrentPlayer(false);
-            setPlayer1([...player1, e.target.id])
-            console.log(player1)
-        } else {
-          console.log("player 2:");
-            setCurrentPlayer(true);
-            setPlayer2([...player2, e.target.id])
-        }
-    };
-
-    let boxes = [];
-    for (let i = 0; i < 9; i++) {
-        boxes.push("hej" + i);
+  const handleClick = (id) => {
+    console.log(id);
+    if (currentPlayer) {
+      console.log("player 1:");
+      setCurrentPlayer(false);
+      setPlayer1([...player1, id]);
+      console.log(player1);
+      setTurns(turns + 1);
+    } else {
+      console.log("player 2:");
+      setCurrentPlayer(true);
+      setPlayer2([...player2, id]);
+      setTurns(turns + 1);
     }
+  };
 
-    return (
-        <>
-            <div className="box-list">
-                {boxes &&
-                    boxes.map((box, i) => {
-                        return <Box key={i} id={i + 1} onClick={handleClick} />;
-                    })}
-            </div>
-        </>
-    );
+  function handleTurns(id) {
+    if (turns % 2 === 0) {
+      player1;
+    } else console.log("player två");
+  }
+
+  let boxes = [];
+  for (let i = 0; i < 9; i++) {
+    boxes.push("hej" + i);
+  }
+
+  return (
+    <>
+      <div className="box-list">
+        <p>Turns: {turns}</p>
+        {boxes &&
+          boxes.map((box, i) => {
+            return (
+              <Box
+                key={i}
+                id={i + 1}
+                onClick={handleClick}
+                currentPlayer={currentPlayer}
+              />
+            );
+          })}
+      </div>
+    </>
+  );
 }
 
 export default Game;
