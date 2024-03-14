a1385ca4923a5e2c1fe6ec648e80eeb771968ac9
dens0n <156082076+dens0n@users.noreply.github.com>
2024-03-14 14:20:24 +0100

    format

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -3,10 +3,10 @@ import { useState } from "react";
 
 function Box({ id, onClick, currentPlayer, marker }) {
     return (
-    <div className="box" id={id} onClick={() => onClick(id)}>
-      {marker}
-    </div>
-  );
+        <div className="box" id={id} onClick={() => onClick(id)}>
+            {marker}
+        </div>
+    );
 }
 
 export default Box;
